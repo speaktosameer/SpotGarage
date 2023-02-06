e4ebc583853edfe9f34bec6a0d09c2afe303d28d
speaktosameer <speaktosameernp@gmail.com>
2023-02-06 12:41:45 +0545

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      < Navbar/>
     </div>
   );
 }
